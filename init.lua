23994de2f0905b4ae221a122410fd825bd514387
Shivam Ashtikar <lightningshivam@gmail.com>
2022-07-19 23:37:12 +0530

    Fixed brokern lualine

--- init.lua
+++ init.lua
@@ -10,6 +10,18 @@ local cmd = vim.cmd -- execute Vim commands
 local g = vim.g -- global variables
 
 
+require("onedark").setup({
+   comment_style = "italic",
+  -- Overwrite the highlight groups
+  overrides = function(c)
+    return {
+      MatchParen = { fg = c.cyan0, bg = c.bg_visual, style = "bold "}
+    }
+  end,
+  colors = {
+    bg_visual =  '#5c6370'
+  }
+})
 cmd[[colorscheme onedark]]
 
 require('options')
