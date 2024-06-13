7f72c66143410b78cc354c319a612e24a67104c6
Shivam Ashtikar <shivam.ashtikar@juspay.in>
2024-06-13 17:41:42 +0530

    added purescript in treesitter

--- plugin/nvim-treesitter.configs.lua
+++ plugin/nvim-treesitter.configs.lua
@@ -1,7 +1,7 @@
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "javascript", "json", "bash", "haskell"},
+  ensure_installed = { "c", "lua", "rust", "javascript", "json", "bash", "haskell", "purescript"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
