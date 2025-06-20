3691595bc8fcd0b5ce59115c3215115ba1eaf4e6
Shivam Ashtikar <lightningshivam@gmail.com>
2025-06-21 02:41:31 +0530

    updated vscode settings

--- setup-vscode.sh
+++ setup-vscode.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -x
 
-mkdir -p $HOME/Library/Application\ Support/Code/User/
-ln -s $PWD/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-ln -s $PWD/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+
+if [ -d "$HOME/Library/Application\ Support/" ]; then
+  VSCODE_DIR=$HOME/Library/Application\ Support/Code/User
+else
+  VSCODE_DIR=$HOME/.config/Code/User
+fi
+mkdir -p $VSCODE_DIR
+ln -s $PWD/vscode/keybindings.json $VSCODE_DIR/keybindings.json
+ln -s $PWD/vscode/settings.json $VSCODE_DIR/settings.json
