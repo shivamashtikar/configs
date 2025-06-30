b9ef6d8f95d22cbf4a72b789299975c207bc0aeb
Shivam Ashtikar <shivam.ashtikar@juspay.in>
2025-06-30 17:08:04 +0530

    auto push config in git

--- setup-gitconfig.sh
+++ setup-gitconfig.sh
@@ -3,6 +3,7 @@ set -x
 
 git config --global core.ignorecase false
 git config --global core.editor nvim
+git config --global push.autoSetupRemote true
 
 read -p "Enter your name: " INPUT_NAME
 git config --global user.name "$INPUT_NAME"
