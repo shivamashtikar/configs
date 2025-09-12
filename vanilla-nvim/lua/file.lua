46f9732a5acfd18d96714a66b7ce7af0cef0f62f
Shivam Ashtikar <lightningshivam@gmail.com>
2025-09-13 01:44:48 +0530

    feat(netrw): remember and restore last buffer after closing Explore
    - Store the current buffer id in `last_buf` when the Explore command is invoked.
    - Detect when the netrw window is closed and automatically restore the stored buffer.
    - Restore only if the stored buffer is still valid, preventing errors.
    - Enhances user experience by returning to the previous context instead of the last used buffer.

--- vanilla-nvim/lua/file.lua
+++ vanilla-nvim/lua/file.lua
@@ -1,12 +1,15 @@
 local u = require('my-utils')
 local nmap = u.nmap
 
+local last_buf = nil
+
 vim.api.nvim_set_keymap('n', '<leader><space>', '', {
   noremap = true,
   silent = true,
   callback = function()
     local api = vim.api
 
+    -- Find netrw window if open
     local netrw_win = nil
     for _, win in ipairs(api.nvim_list_wins()) do
       local buf_name = api.nvim_buf_get_name(api.nvim_win_get_buf(win))
@@ -17,9 +20,17 @@ vim.api.nvim_set_keymap('n', '<leader><space>', '', {
     end
 
     if netrw_win then
+      -- Close netrw window
       api.nvim_win_close(netrw_win, true)
-      vim.cmd('b#')
+      -- Switch back to the buffer we stored last time
+      if last_buf and api.nvim_buf_is_valid(last_buf) then
+        api.nvim_set_current_buf(last_buf)
+      end
+      last_buf = nil
     else
+      -- Store current buffer before opening Explore
+      last_buf = api.nvim_get_current_buf()
+      -- Open Explore window
       vim.cmd('Explore')
     end
   end,
