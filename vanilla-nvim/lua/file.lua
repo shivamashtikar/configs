0f612556f9c4fc7ba3a747fe11fe92208df6b4dc
Shivam Ashtikar <lightningshivam@gmail.com>
2025-09-13 01:41:37 +0530

    refactor(keymaps): simplify toggle_explore mapping
    - Remove old `toggle_explore` function and its separate `nmap` call
    - Implement toggle logic directly in a single `vim.api.nvim_set_keymap` callback
    - Eliminate unused `fn` variable
    - Shorten the netrw detection loop
    - Preserve the `<leader><space>` mapping in normal mode
    - Remove global helper, streamlining the file structure

--- vanilla-nvim/lua/file.lua
+++ vanilla-nvim/lua/file.lua
@@ -1,35 +1,29 @@
 local u = require('my-utils')
 local nmap = u.nmap
 
-local function toggle_explore()
-  local api = vim.api
-  local bufnr = api.nvim_get_current_buf()
-  local fn = vim.fn
+vim.api.nvim_set_keymap('n', '<leader><space>', '', {
+  noremap = true,
+  silent = true,
+  callback = function()
+    local api = vim.api
 
-  -- Check if a netrw buffer is already open in any window
-  local netrw_win = nil
-  for _, win in ipairs(api.nvim_list_wins()) do
-    local win_bufnr = api.nvim_win_get_buf(win)
-    local buf_name = api.nvim_buf_get_name(win_bufnr)
-    -- Netrw buffers have "netrw://" in their name
-    if buf_name:match("netrw://") then
-      netrw_win = win
-      break
+    local netrw_win = nil
+    for _, win in ipairs(api.nvim_list_wins()) do
+      local buf_name = api.nvim_buf_get_name(api.nvim_win_get_buf(win))
+      if buf_name:match("netrw://") then
+        netrw_win = win
+        break
+      end
     end
-  end
 
-  if netrw_win then
-    -- netrw is open; close its window and return to previous buffer
-    api.nvim_win_close(netrw_win, true)
-    -- Switch back to previous buffer (last edited file)
-    vim.cmd('b#')
-  else
-    -- netrw not open; open it
-    vim.cmd('Explore')
-  end
-end
+    if netrw_win then
+      api.nvim_win_close(netrw_win, true)
+      vim.cmd('b#')
+    else
+      vim.cmd('Explore')
+    end
+  end,
+})
 
--- Map <leader><space> to toggle function in normal mode
-nmap('<leader><space>', "<cmd>lua toggle_explore()<CR>")
 
 nmap('<leader>ff', ':find ')
