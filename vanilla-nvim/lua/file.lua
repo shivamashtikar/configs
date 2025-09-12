176e5c60adff2b70ec718eba27549a5bd272fef5
Shivam Ashtikar <lightningshivam@gmail.com>
2025-09-13 02:09:32 +0530

    fix(config): update <leader>ff keybinding to use :find
    - Change mapping from `:files<CR>:file` to `:files<CR>:find`
    - Ensure file picker opens the correct `:find` command
    - Remove deprecated use of `:file` after file list display

--- vanilla-nvim/lua/file.lua
+++ vanilla-nvim/lua/file.lua
@@ -2,6 +2,6 @@ local u = require('my-utils')
 local nmap = u.nmap
 
 nmap('<leader><space>',':Explore<CR>')
-nmap('<leader>ff', ':files<CR>:file<Space>')
+nmap('<leader>ff', ':files<CR>:find<Space>')
 nmap('<leader>fe', ':buffers<CR>:buffer<Space>')
 nmap('<leader>fc', ":argadd <c-r>=fnameescape(expand('%:p:h'))<cr>/*<C-d>")
