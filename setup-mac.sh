720ccea7decf255b2e4ff4159de2035aea3ae06d
Shivam Ashtikar <lightningshivam@gmail.com>
2026-03-13 01:55:18 +0530

    feat: added zed config, using yazi for directory movement

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -x
 
-brew install ranger
+
+brew install neovim tmux lolcat figlet ranger
+
 brew install yazi ffmpeg-full sevenzip jq poppler fd ripgrep fzf zoxide resvg imagemagick-full font-symbols-only-nerd-font
+
+brew install lua-language-server bash-language-server
+
